c03ebbe7d5ec217f589e02da9ccdb5974ce30a69
Jawad Ahmed <jawadahmed.tdc@gmail.com>
2019-10-02 11:29:25 +0500

    General Ledger Controller

---
+++ Taxtation/Controllers/GeneralLedgerController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Taxtation.Controllers
+{
+    public class GeneralLedgerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
